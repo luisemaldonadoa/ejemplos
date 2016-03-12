823fcd3fd7df45edde256560ed658aa405157159
luismaldonado <luis.maldonado@maices.com>
2016-03-12 17:35:43 -0600

    se agrega proyecto volley pero no funciono tal ves luego sea eliminado, pero se a grego la libreria de volley al proyecto httpmethods se elimino todo lo inecesario dejando una solo peticion muy simple

--- android/HttpMethods/app/src/main/java/com/example/luisenrique/httpmethods/HttpMethodsActivity.java
+++ android/HttpMethods/app/src/main/java/com/example/luisenrique/httpmethods/HttpMethodsActivity.java
@@ -1,13 +1,68 @@
 package com.example.luisenrique.httpmethods;
 
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.http.client.methods.HttpGetHC4;
+import org.apache.http.impl.DefaultBHttpClientConnection;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HttpMethodsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_http_methods);
+
+        String url = "http://httpbin.org/html";
+
+// Request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        // Result handling
+                        //System.out.println(response.substring(0,500));
+                        System.out.println(response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                // Error handling
+                System.out.println("Something went wrong!");
+                error.printStackTrace();
+
+            }
+        });
+        // Add the request to the queue
+        Volley.newRequestQueue(this).add(stringRequest);
+
     }
+
+
 }
